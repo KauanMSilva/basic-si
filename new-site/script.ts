b34f2be4e9db73720d8756871b381ad6c08ab6f4
Kauan Mariano Silva <144391114+KauanMSilva@users.noreply.github.com>
2025-01-08 17:58:16 -0300

    Update script.ts

--- new-site/script.ts
+++ new-site/script.ts
@@ -1,8 +1,8 @@
-// Seleciona o botão e o parágrafo
+
 const botao = document.getElementById('botao') as HTMLButtonElement;
 const mensagem = document.getElementById('mensagem') as HTMLParagraphElement;
 
-// Adiciona evento de clique ao botão
+
 botao.addEventListener('click', () => {
   mensagem.textContent = 'Olá, mundo!';
 });
